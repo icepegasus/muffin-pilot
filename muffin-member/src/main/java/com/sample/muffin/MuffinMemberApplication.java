e60f74d82c8123a95a9d782c3a69d97082587a9b
icepega <icepega@gmail.com>
2020-10-01 19:03:48 +0900

    swagger 및 spring cloud 동시적용 버전 임시

--- muffin-member/src/main/java/com/sample/muffin/MuffinMemberApplication.java
+++ muffin-member/src/main/java/com/sample/muffin/MuffinMemberApplication.java
@@ -2,8 +2,11 @@ package com.sample.muffin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
 
 @SpringBootApplication
+@EnableBinding(Processor.class)
 public class MuffinMemberApplication {
 
 	public static void main(String[] args) {
