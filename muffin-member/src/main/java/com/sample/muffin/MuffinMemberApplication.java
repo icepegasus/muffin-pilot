837f9c724a82ebf5e7fe88d12b402473872e0c8b
icepega <icepega@gmail.com>
2020-10-01 19:05:00 +0900

    Revert "swagger 및 spring cloud 동시적용 버전 임시"
    This reverts commit e60f74d82c8123a95a9d782c3a69d97082587a9b.

--- muffin-member/src/main/java/com/sample/muffin/MuffinMemberApplication.java
+++ muffin-member/src/main/java/com/sample/muffin/MuffinMemberApplication.java
@@ -2,11 +2,8 @@ package com.sample.muffin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Processor;
 
 @SpringBootApplication
-@EnableBinding(Processor.class)
 public class MuffinMemberApplication {
 
 	public static void main(String[] args) {
